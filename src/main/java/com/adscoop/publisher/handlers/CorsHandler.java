068337c69a7d547e34c7cc111a71f8c36a0292eb
= <=>
2017-07-30 19:50:33 +0200

    fixed cors header

--- src/main/java/com/adscoop/publisher/handlers/CorsHandler.java
+++ src/main/java/com/adscoop/publisher/handlers/CorsHandler.java
@@ -14,11 +14,12 @@ public class CorsHandler implements Handler {
     @Override
     public void handle(Context ctx) throws Exception {
         MutableHeaders headers = ctx.getResponse().getHeaders();
-
-        headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,ctx.getRequest().getHeaders().get("Origin"));
+        headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,"*");
     headers.set(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "*");
-    headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS , true);
 
+    headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS , true);
+    headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET,HEAD,OPTIONS,POST,PUT,PATCH");
+headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Cache-Control, withCredentials, Last-Event-ID ");
 
 
 
